30ff898c6dda55c72efb12d647b5998988559d42
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2019-07-21 13:51:06 +0530

    Add files via upload

---
+++ forms/search/SearchScholarshipTransaction.cs
@@ -0,0 +1,419 @@
+﻿using Finance_Management.forms.Navigation;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Finance_Management.forms.Search
+{
+    public partial class SearchScholarshipTransaction : Form
+    {
+        private SQL_Operator sql_operator = new SQL_Operator();
+        private Validation validation = new Validation();
+        private StudentTransactionSearchMenu studentTransactionSearchMenu;
+        private TimeListAdder timeListAdder = new TimeListAdder();
+        private DateTime today = DateTime.Now;
+        private Regex usnExp = new Regex("^[A-Z1-9]+$");
+        private Regex scholarshipNameExp = new Regex("^[A-Z /]+$");
+
+        public SearchScholarshipTransaction(StudentTransactionSearchMenu studentTransactionSearchMenu)
+        {
+            InitializeComponent();
+            this.studentTransactionSearchMenu = studentTransactionSearchMenu;
+        }
+
+        private void SearchScholarshipTransaction_Load(object sender, EventArgs e)
+        {
+            for (int year = today.Year; year >= 2015; year--)
+            {
+                YearList.Items.Add(year);
+                YearToList.Items.Add(year);
+            }
+        }
+
+        private void LabelName_Click(object sender, EventArgs e)
+        {
+            NameBox.Select();
+        }
+
+        private void LabelUSN_Click(object sender, EventArgs e)
+        {
+            USNBox.Select();
+        }
+
+        private void LabelScholarshipName_Click(object sender, EventArgs e)
+        {
+            ScholarshipNameBox.Select();
+        }
+
+        private void LabelAmount_Click(object sender, EventArgs e)
+        {
+            AmountBox.Select();
+        }
+
+        private void LabelAmountTo_Click(object sender, EventArgs e)
+        {
+            AmountToBox.Select();
+        }
+
+        private string Get_Search_Query()
+        {
+            string sqlQuery = "SELECT name Name, st.usn USN, scholarship_name \"Scholarship Name\", amount Amount, TO_CHAR(time, 'YYYY-MM-DD HH24:MI') Time FROM SCHOLARSHIP_TRANSACTION st LEFT JOIN STUDENT_INFO si ON st.usn = si.usn LEFT JOIN SCHOLARSHIP_TYPE sct ON st.scholarship_id = sct.scholarship_id";
+            string startTimestamp = validation.Get_Timestamp(YearList, MonthList, DayList, HourList, MinuteList, AMPMList);
+            string endTimestamp = validation.Get_Timestamp(YearToList, MonthToList, DayToList, HourToList, MinuteToList, AMPMToList);
+            bool criteria = false;
+
+            if (validation.Name(NameBox.Text.ToUpper()))
+            {
+                sqlQuery += " WHERE name LIKE '%" + NameBox.Text.ToUpper() + "%'";
+                criteria = true;
+            }
+
+            if (usnExp.Match(USNBox.Text.ToUpper()).Success)
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND st.usn LIKE '%" + USNBox.Text.ToUpper() + "%'";
+                }
+                else
+                {
+                    sqlQuery += " WHERE st.usn LIKE '%" + USNBox.Text.ToUpper() + "%'";
+                    criteria = true;
+                }
+            }
+
+            if (scholarshipNameExp.Match(ScholarshipNameBox.Text.ToUpper()).Success)
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND sct.scholarship_name LIKE '%" + ScholarshipNameBox.Text.ToUpper() + "%'";
+                }
+                else
+                {
+                    sqlQuery += " WHERE sct.scholarship_name LIKE '%" + ScholarshipNameBox.Text.ToUpper() + "%'";
+                    criteria = true;
+                }
+            }
+
+            if (validation.Amount(AmountBox.Text) && validation.Amount(AmountToBox.Text))
+            {
+                if (int.Parse(AmountBox.Text) > int.Parse(AmountToBox.Text))
+                {
+                    string temp = AmountBox.Text;
+                    AmountBox.Text = AmountToBox.Text;
+                    AmountToBox.Text = temp;
+                }
+            }
+
+            if (validation.Amount(AmountBox.Text))
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND amount < -" + AmountBox.Text + "";
+                }
+                else
+                {
+                    sqlQuery += " WHERE amount < -" + AmountBox.Text + "";
+                    criteria = true;
+                }
+            }
+            if (validation.Amount(AmountToBox.Text))
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND amount > -" + AmountToBox.Text + "";
+                }
+                else
+                {
+                    sqlQuery += " WHERE amount > -" + AmountToBox.Text + "";
+                    criteria = true;
+                }
+            }
+
+            if (startTimestamp != "")
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND TO_CHAR(time, 'YYYY-MM-DD HH24:MI') > '" + startTimestamp + "'";
+                }
+                else
+                {
+                    sqlQuery += " WHERE TO_CHAR(time, 'YYYY-MM-DD HH24:MI') > '" + startTimestamp + "'";
+                    criteria = true;
+                }
+            }
+            if (endTimestamp != "")
+            {
+                if (criteria)
+                {
+                    sqlQuery += " AND TO_CHAR(time, 'YYYY-MM-DD HH24:MI') < '" + endTimestamp + "'";
+                }
+                else
+                {
+                    sqlQuery += " WHERE TO_CHAR(time, 'YYYY-MM-DD HH24:MI') < '" + endTimestamp + "'";
+                    criteria = true;
+                }
+            }
+            sqlQuery += " ORDER BY time DESC";
+            return sqlQuery;
+        }
+
+        private void NameBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private void USNBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private void ScholarshipNameBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private void AmountBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private void AmountToBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                string sqlQuery = Get_Search_Query();
+                sql_operator.Display(sqlQuery, DataView);
+            }
+        }
+
+        private void Clear_All_Entries()
+        {
+            NameBox.Clear();
+            USNBox.Clear();
+            ScholarshipNameBox.Clear();
+            AmountBox.Clear();
+            AmountToBox.Clear();
+            YearList.SelectedItems.Clear();
+            YearToList.SelectedItems.Clear();
+            MonthList.Visible = false;
+            MonthToList.Visible = false;
+            DayList.Visible = false;
+            DayToList.Visible = false;
+            HourList.Visible = false;
+            HourToList.Visible = false;
+            MinuteList.Visible = false;
+            MinuteToList.Visible = false;
+            AMPMList.Visible = false;
+            AMPMToList.Visible = false;
+            DataView.DataSource = null;
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = Get_Search_Query();
+            sql_operator.Display(sqlQuery, DataView);
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            studentTransactionSearchMenu.Show();
+            Hide();
+            Clear_All_Entries();
+        }
+
+        private void SearchScholarshipTransaction_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            studentTransactionSearchMenu.Show();
+            Hide();
+            Clear_All_Entries();
+        }
+
+        private void YearList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MonthList.Visible = false;
+            MonthList.ClearSelected();
+            MonthList.Items.Clear();
+            if (YearList.Text != "")
+            {
+                if (int.Parse(YearList.Text) == today.Year)
+                {
+                    timeListAdder.Fill_Month(MonthList, today.Month);
+                }
+                else
+                {
+                    timeListAdder.Fill_Month(MonthList, 12);
+                }
+                MonthList.Visible = true;
+            }
+        }
+
+        private void YearToList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MonthToList.Visible = false;
+            MonthToList.ClearSelected();
+            MonthToList.Items.Clear();
+            if (YearToList.Text != "")
+            {
+                if (int.Parse(YearToList.Text) == today.Year)
+                {
+                    timeListAdder.Fill_Month(MonthToList, today.Month);
+                }
+                else
+                {
+                    timeListAdder.Fill_Month(MonthToList, 12);
+                }
+                MonthToList.Visible = true;
+            }
+        }
+
+        private void MonthList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DayList.Visible = false;
+            DayList.ClearSelected();
+            DayList.Items.Clear();
+            if (MonthList.Text != "")
+            {
+                if (int.Parse(YearList.Text) == today.Year && int.Parse(MonthList.Text) == 2)
+                {
+                    timeListAdder.Fill_Day(DayList, today.Day);
+                }
+                else
+                {
+                    timeListAdder.Fill_Day(DayList, validation.Maximum_Days_Month(int.Parse(MonthList.Text), int.Parse(YearList.Text)));
+                }
+                DayList.Visible = true;
+            }
+        }
+
+        private void MonthToList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DayToList.Visible = false;
+            DayToList.ClearSelected();
+            DayToList.Items.Clear();
+            if (MonthToList.Text != "")
+            {
+                if (int.Parse(YearToList.Text) == today.Year && int.Parse(MonthToList.Text) == 2)
+                {
+                    timeListAdder.Fill_Day(DayToList, today.Day);
+                }
+                else
+                {
+                    timeListAdder.Fill_Day(DayToList, validation.Maximum_Days_Month(int.Parse(MonthToList.Text), int.Parse(YearToList.Text)));
+                }
+                DayToList.Visible = true;
+            }
+        }
+
+        private void DayList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HourList.Visible = false;
+            HourList.ClearSelected();
+            HourList.Items.Clear();
+            if (DayList.Text != "")
+            {
+                if (int.Parse(YearList.Text) == today.Year && int.Parse(MonthList.Text) == today.Month && int.Parse(DayList.Text) == today.Day)
+                {
+                    timeListAdder.Fill_Hour(HourList, today.Hour % 12);
+                }
+                else
+                {
+                    timeListAdder.Fill_Hour(HourList, 11);
+                }
+                HourList.Visible = true;
+            }
+        }
+
+        private void DayToList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HourToList.Visible = false;
+            HourToList.ClearSelected();
+            HourToList.Items.Clear();
+            if (DayToList.Text != "")
+            {
+                if (int.Parse(YearToList.Text) == today.Year && int.Parse(MonthToList.Text) == today.Month && int.Parse(DayToList.Text) == today.Day)
+                {
+                    timeListAdder.Fill_Hour(HourToList, today.Hour % 12);
+                }
+                else
+                {
+                    timeListAdder.Fill_Hour(HourToList, 11);
+                }
+                HourToList.Visible = true;
+            }
+        }
+
+        private void HourList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MinuteList.Visible = false;
+            AMPMList.Visible = false;
+            MinuteList.ClearSelected();
+            AMPMList.ClearSelected();
+            if (HourList.Text != "")
+            {
+                if (int.Parse(YearList.Text) == today.Year && int.Parse(MonthList.Text) == today.Month && int.Parse(DayList.Text) == today.Day && int.Parse(HourList.Text) == today.Hour)
+                {
+                    MinuteList.Items.Clear();
+                    timeListAdder.Fil_Minute(MinuteList, today.Minute);
+                    MinuteList.Text = "00";
+                }
+                else if (MinuteList.Text == "")
+                {
+                    timeListAdder.Fil_Minute(MinuteList, 59);
+                    MinuteList.Text = "00";
+                }
+                if (AMPMList.Text == "")
+                {
+                    AMPMList.Text = "AM";
+                }
+                MinuteList.Visible = true;
+                AMPMList.Visible = true;
+            }
+        }
+
+        private void HourToList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MinuteToList.Visible = false;
+            AMPMToList.Visible = false;
+            MinuteToList.ClearSelected();
+            AMPMToList.ClearSelected();
+            if (HourToList.Text != "")
+            {
+                if (int.Parse(YearToList.Text) == today.Year && int.Parse(MonthToList.Text) == today.Month && int.Parse(DayToList.Text) == today.Day && int.Parse(HourToList.Text) == today.Hour)
+                {
+                    MinuteToList.Items.Clear();
+                    timeListAdder.Fil_Minute(MinuteToList, today.Minute);
+                    MinuteToList.Text = "00";
+                }
+                else if (MinuteToList.Text == "")
+                {
+                    timeListAdder.Fil_Minute(MinuteToList, 59);
+                    MinuteToList.Text = "00";
+                }
+
+                if (AMPMToList.Text == "")
+                {
+                    AMPMToList.Text = "AM";
+                }
+                MinuteToList.Visible = true;
+                AMPMToList.Visible = true;
+            }
+        }
+    }
+}
